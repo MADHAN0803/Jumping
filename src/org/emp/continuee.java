c06135070456a33b142acdac4b5145267e1f9448
MADHAN0803 <madhanrugu555@gmail.com>
2022-07-10 19:41:56 +0530

    Amma and Appa

---
+++ src/org/emp/continuee.java
@@ -0,0 +1,11 @@
+package org.emp;
+
+public class continuee {
+	public static void main(String[] args) {
+		for(int i =0;i<=10;i++) {
+			if(i==4) {
+				System.exit(0);
+			}System.out.println(i);
+		}System.out.println("end");
+	}
+}
---
+++ src/org/emp/reak.java
@@ -0,0 +1,18 @@
+package org.emp;
+
+public class reak {
+	
+	public static void main(String[] args) {
+		
+		 for(int i = 0;i<=10;i++) {
+			 
+			 if(i==4) {
+				 
+			break;	
+				
+			 }System.out.println(i);
+			 
+		 }System.out.println("end");
+	}
+
+}
